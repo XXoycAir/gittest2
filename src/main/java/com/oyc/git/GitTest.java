ad61bdef4ed7ba295dc1136d5ae44a8bf24979e4
paidaxing <314357698@qq.com>
2024-04-08 23:32:42 +0800

    hot-fix 666

--- src/main/java/com/oyc/git/GitTest.java
+++ src/main/java/com/oyc/git/GitTest.java
@@ -6,11 +6,5 @@ public class GitTest {
     public static void main(String[] args) {
 
 
-        System.out.println("nihao");
-        System.out.println("nihao,git3");
-        System.out.println("我是fix-out");
-
-        System.out.println("11111");
-        System.out.println("11111");
     }
 }
