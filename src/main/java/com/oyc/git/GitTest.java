dbab9929f0a5597bd4a9ced8d5dfeb50d782797e
paidaxing <314357698@qq.com>
2024-04-08 22:46:00 +0800

    second commit

--- src/main/java/com/oyc/git/GitTest.java
+++ src/main/java/com/oyc/git/GitTest.java
@@ -1,11 +1,11 @@
 package com.oyc.git;
 
-import java.awt.print.Printable;
 
 public class GitTest {
     public static void main(String[] args) {
 
 
+        System.out.println("nihao");
         System.out.println("nihao");
 
     }
