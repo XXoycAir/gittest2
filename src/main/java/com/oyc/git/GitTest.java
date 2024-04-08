b0549044196fbbc498306b49b75f40714b5156dd
paidaxing <314357698@qq.com>
2024-04-08 23:31:55 +0800

    master 666

--- src/main/java/com/oyc/git/GitTest.java
+++ src/main/java/com/oyc/git/GitTest.java
@@ -6,9 +6,6 @@ public class GitTest {
     public static void main(String[] args) {
 
 
-        System.out.println("nihao");
-        System.out.println("nihao,git3");
-        System.out.println("我是fix-out");
 
     }
 }
